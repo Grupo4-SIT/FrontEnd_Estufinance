e0e032bfb510b6afe1d6d375cc6c280bafea8cc7
Joel Mendoza <choy98@outlook.com.pe>
2023-06-26 04:03:56 -0500

    Configuramos el formulario para agregar movimientos reutilizados tanto para entrada como para salida.

--- client-finance-app-main/src/environments/environment.development.ts
+++ client-finance-app-main/src/environments/environment.development.ts
@@ -1,4 +1,8 @@
 export const environmentDev = {
-    production: true,
-    url: "http://localhost:14156/"
-  };
+  production: true,
+  url: "http://localhost:14156/"
+};
+export const environmentJsonDev = {
+  production: true,
+  url: "../assets/json/logo-concepto/logo.json"
+};
