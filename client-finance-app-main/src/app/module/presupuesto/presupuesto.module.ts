53534a3aeff91cda5f18635fc9e36182b4026f27
Joel Mendoza <choy98@outlook.com.pe>
2023-06-26 05:28:12 -0500

    Hemos configurado el interceptor y hemos agregado al formulario de movimiento la posibilidad de agregar movimientos con presupuesto asociado.

--- client-finance-app-main/src/app/module/presupuesto/presupuesto.module.ts
+++ client-finance-app-main/src/app/module/presupuesto/presupuesto.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PresupuestoRoutingModule } from './presupuesto-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 import { PresupuestoComponent } from './pages/presupuesto.component';
 import { MetricasComponent } from './components/metricas/metricas.component';
 import { DetallesComponent } from './components/detalles/detalles.component';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { FormPresupuestoComponent } from './components/crear-presupuesto/form-presupuesto/form-presupuesto.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     PresupuestoComponent,
     MetricasComponent,
-    DetallesComponent
+    DetallesComponent,
+    FormPresupuestoComponent
   ],
   imports: [
     CommonModule,
     PresupuestoRoutingModule,
+    ReactiveFormsModule,
     SharedModule
   ]
 })
