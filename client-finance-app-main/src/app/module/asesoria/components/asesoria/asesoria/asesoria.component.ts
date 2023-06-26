5d1760974f116b81e68dd1cc04516abfc7330575
Joel Mendoza <choy98@outlook.com.pe>
2023-06-26 14:20:02 -0500

    Hemos configurado la vista del asistente virtual y su diseño.

--- client-finance-app-main/src/app/module/asesoria/components/asesoria/asesoria/asesoria.component.ts
+++ client-finance-app-main/src/app/module/asesoria/components/asesoria/asesoria/asesoria.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../../../../../shared/services/asesoria/chatGpt/chat.service';
 
 @Component({
   selector: 'app-asesoria',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./asesoria.component.scss']
 })
 export class AsesorComponent {
+  estadoEnvio: boolean = false;
+  mensaje: string = '';
+  mensajeChat: string = '';
+  mensajePila: string[] = [];
 
+  constructor(private _chatService: ChatService) { }
+
+  async submit() {
+    this.mensajePila.push(this.mensaje); // Agrega el mensaje del usuario a la variable mensaje
+    this.estadoEnvio = true;
+    this._chatService.getMessageChat(this.mensaje).subscribe(
+      {
+        next: (value: any) => {
+          this.mensajeChat = value.anwer;
+          this.mensajePila.push(this.mensajeChat); // Agrega el mensaje del asistente a la lista
+          this.mensaje = ''; // Limpia el campo de texto del usuario
+          this.estadoEnvio = false;
+        },
+        error: (err: any) => {
+          console.log(err)
+          this.mensajeChat = err;
+          this.mensajePila.push(this.mensajeChat); // Agrega el mensaje del asistente a la lista
+          this.mensaje = ''; // Limpia el campo de texto del usuario
+        }
+      }
+    )
+  }
 }
+
