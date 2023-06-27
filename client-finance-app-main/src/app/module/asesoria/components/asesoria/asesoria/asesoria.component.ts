a7fb4a4f6f974f67c797a0f459e20f1f90cdf656
DarkOvs77 <75710806+DarkOverlord689@users.noreply.github.com>
2023-06-27 01:12:10 -0500

    Configuramos el formulario para agregar inversiones

--- client-finance-app-main/src/app/module/asesoria/components/asesoria/asesoria/asesoria.component.ts
+++ client-finance-app-main/src/app/module/asesoria/components/asesoria/asesoria/asesoria.component.ts
@@ -7,32 +7,45 @@ import { ChatService } from '../../../../../shared/services/asesoria/chatGpt/cha
   styleUrls: ['./asesoria.component.scss']
 })
 export class AsesorComponent {
-  estadoEnvio: boolean = false;
+
   mensaje: string = '';
   mensajeChat: string = '';
   mensajePila: string[] = [];
 
   constructor(private _chatService: ChatService) { }
 
-  async submit() {
+  submit() {
     this.mensajePila.push(this.mensaje); // Agrega el mensaje del usuario a la variable mensaje
-    this.estadoEnvio = true;
-    this._chatService.getMessageChat(this.mensaje).subscribe(
-      {
-        next: (value: any) => {
-          this.mensajeChat = value.anwer;
-          this.mensajePila.push(this.mensajeChat); // Agrega el mensaje del asistente a la lista
-          this.mensaje = ''; // Limpia el campo de texto del usuario
-          this.estadoEnvio = false;
-        },
-        error: (err: any) => {
-          console.log(err)
-          this.mensajeChat = err;
-          this.mensajePila.push(this.mensajeChat); // Agrega el mensaje del asistente a la lista
-          this.mensaje = ''; // Limpia el campo de texto del usuario
+    // ... lógica para procesar la respuesta del asistente ...
+    this.obtenerMensajeChat(this.mensaje)
+      .then((value: string) => {
+        this.mensajeChat = value;
+      })
+      .catch((reason: any) => {
+        console.log(reason)
+      });
+    // this.mensajeChat añadir a la lista
+    this.mensajePila.push("Respuestas")
+    this.mensaje = ''; // Limpia el campo de texto del usuario
+  }
+
+  async obtenerMensajeChat(message: string): Promise<string> {
+    const respuesta = await new Promise<string>((resolve, reject) => {
+      this._chatService.getMessageChat(message).subscribe(
+        {
+          next: (value: string) => {
+            resolve(value);
+          },
+          error: (err: any) => {
+            reject(err)
+          },
+          complete: () => {
+            //
+          }
         }
-      }
-    )
+      )
+    })
+    return respuesta
   }
 }
 
